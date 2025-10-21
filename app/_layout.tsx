945a8a612fddce4f4f5051a6373e991182a1edca
nyolay <nyolay125ja@gmail.com>
2025-10-21 21:29:11 +0630

    replace some

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,7 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack initialRouteName="kyaw1">
+    <Stack.Screen name="kyaw1" options={{headerShown:false}}></Stack.Screen>
+  </Stack>;
 }
